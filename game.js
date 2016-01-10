ad9b47475224f826d5184b8f005476c4bd9f9056
Sarah Wong <sarahlt.wong@outlook.com>
2016-01-10 15:39:33 -0800

    Added week 1 class 3 assignment

---
+++ game.js
@@ -0,0 +1,38 @@
+//score variable counts the # of correct answers
+      var score = 0;
+//question one
+      var q1 = prompt ("Do I like lattes?").toLowerCase();
+      if(q1 === "yes" || q1 === "y"){
+        score++;
+        console.log("That is correct!");
+      } else if (q1 === "no" || q1 === "n") {
+        console.log("That is incorrect!");
+      }
+//question two
+      var q2 = prompt ("Do I like tea?").toLowerCase();
+      if(q2 === "yes" || q1 === "y"){
+        score++;
+        console.log("That is correct!");
+      } else if (q2 === "no" || q1 === "n") {
+        console.log("That is incorrect!");
+      }
+//question three
+      var q3 = prompt ("Do I like dogs?").toLowerCase();
+      if(q3 === "yes" || q1 === "y"){
+        score++;
+        console.log("That is correct!");
+      } else if (q3 === "no" || q1 === "n") {
+        console.log("That is incorrect!");
+      }
+//question four
+      var q4 = prompt ("How old am I?");
+      if(q4 > 24){
+        console.log("That is too high!");
+      } else if (q4 < 24) {
+        console.log("That is too low!");
+      } else if (q4 = 24) {
+        score++;
+        console.log("That is correct!");
+      }
+//print score
+      console.log("You answered " + score + " questions correctly!")
