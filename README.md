cefeddca01120944d2471f1268744026f7c526ff
Sarah Wong <sarahlt.wong@outlook.com>
2016-01-12 18:00:34 -0800

    Added 4th draft of week 1 class 3 assignment

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # codefellows201-guessing-game
 Project name: Guessing Game
+
+
 Project description: This is a guessing game project written for the Code Fellows 201 class.
+
 The user guesses 4 questions and then gets their overall score at the end.
 
 Author: Sarah Wong
-Contact info: https://github.com/sarahltwong
+
+Contact info: [Github](https://github.com/sarahltwong)
--- game.js
+++ game.js
@@ -4,43 +4,46 @@
       var q1 = prompt ("Do I like lattes?").toLowerCase();
       if(q1 === "yes" || q1 === "y"){
         score++;
-        console.log("That is correct!");
+        alert("That is correct!");
       } else if (q1 === "no" || q1 === "n") {
-        console.log("That is incorrect!");
+        alert("That is incorrect!");
       }
 //question two
       var q2 = prompt ("Do I like tea?").toLowerCase();
       if(q2 === "yes" || q1 === "y"){
         score++;
-        console.log("That is correct!");
+        alert("That is correct!");
       } else if (q2 === "no" || q1 === "n") {
-        console.log("That is incorrect!");
+        alert("That is incorrect!");
       }
 //question three
       var q3 = prompt ("Do I like dogs?").toLowerCase();
       if(q3 === "yes" || q1 === "y"){
         score++;
-        console.log("That is correct!");
+        alert("That is correct!");
       } else if (q3 === "no" || q1 === "n") {
-        console.log("That is incorrect!");
+        alert("That is incorrect!");
       }
 //question four
       var q4 = prompt ("How old am I?");
       if(q4 > 24){
-        console.log("That is too high!");
+        alert("That is too high!");
       } else if (q4 < 24) {
-        console.log("That is too low!");
+        alert("That is too low!");
       } else if (q4 == 24) {
         score++;
-        console.log("That is correct!");
+        alert("That is correct!");
       } else if (typeof q4 === "string") {
-        console.log("That is not a number!")
+        alert("That is not a number!");
       }
 //question five
-      do{
-        var q5 = prompt ("What number am I thinking of?")
-      }
-      while(q5 <> 23)
+      var q5 = prompt ("What number am I thinking of? It is below 50.");
+      var q5answer = 23;
+      while (q5 != q5answer){
+        var q5 = prompt ("Nope! Guess again.");
+        }
+          score++;
+          alert("That is correct!");
 
 //print score
-      console.log("You answered " + score + " questions correctly!")
+      alert("You answered " + score + " questions correctly!");
