90bca291e2979a1be8b7be3ee5637034107fed1b
Sarah Wong <sarahlt.wong@outlook.com>
2016-01-14 17:54:48 -0800

    Added class 4 assignment

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ The user guesses 4 questions and then gets their overall score at the end.
 **Author:** Sarah Wong
 
 **Contact info:** [Github](https://github.com/sarahltwong) :space_invader:
+
+**Partner:** Daniel Li
+
+**Contact info:** [Github](https://github.com/danielxli/codefellows201-guessing-game)
--- game.js
+++ game.js
@@ -1,8 +1,11 @@
+var startGame = document.getElementById("startMessage");
+
 var score = 0;
 var questions = [
   "Do I like lattes?",
   "Do I like tea?",
   "Do I like dogs?",
+  "Do I like chocolate?",
   "How old am I?",
   "What is one of my favorite things?"
 ];
@@ -10,73 +13,28 @@ var answers = [
   ["yes","y","yeah"],
   ["yes","y"],
   ["yes","y"],
+  ["yes","y"],
   ["24"],
-  ["raindrops", "roses", "kittens", "chocolate", "flowers", "flannel", "backpacks", "puppies"]
+  ["raindrops", "roses", "kittens", "chocolate", "flowers", "puppies"]
 ];
+
+var answersSubmitted = [];
+
+var statusLog = document.getElementById("printStatus");
+statusLog.textContent = "First question...";
 
 function game(question,answer) {
   var askedQuestion = prompt(question).toLowerCase();
   for (var j = 0; j < answer.length; j++){
     if (answer[j] === askedQuestion){
       score++;
-      console.log("That is correct!");
+      statusLog.textContent = "That is correct! Your score is " + score + ".";
       j = answer.length;
     } else if (j === answer.length - 1){
-      console.log("That is incorrect!");
+      statusLog.textContent = "That is incorrect! Your score is still " + score + ".";;
     }
   }
 }
 for (var i = 0; i < questions.length; i++){
   game(questions[i],answers[i]);
 }
-console.log (score);
-
-// //score variable counts the # of correct answers
-//       var score = 0;
-// //question one
-//       var q1 = prompt ("Do I like lattes?").toLowerCase();
-//       if(q1 === "yes" || q1 === "y"){
-//         score++;
-//         alert("That is correct!");
-//       } else if (q1 === "no" || q1 === "n") {
-//         alert("That is incorrect!");
-//       }
-// //question two
-//       var q2 = prompt ("Do I like tea?").toLowerCase();
-//       if(q2 === "yes" || q1 === "y"){
-//         score++;
-//         alert("That is correct!");
-//       } else if (q2 === "no" || q1 === "n") {
-//         alert("That is incorrect!");
-//       }
-// //question three
-//       var q3 = prompt ("Do I like dogs?").toLowerCase();
-//       if(q3 === "yes" || q1 === "y"){
-//         score++;
-//         alert("That is correct!");
-//       } else if (q3 === "no" || q1 === "n") {
-//         alert("That is incorrect!");
-//       }
-// //question four
-//       var q4 = prompt ("How old am I?");
-//       if(parseInt(q4) > 24){
-//         alert("That is too high!");
-//       } else if (parseInt(q4) < 24) {
-//         alert("That is too low!");
-//       } else if (parseInt(q4) = 24) {
-//         score++;
-//         alert("That is correct!");
-//       } else if (typeof q4 === "string") {
-//         alert("That is not a number!");
-//       }
-// //question five
-//       var q5 = prompt ("What number am I thinking of? It is below 50.");
-//       var q5answer = 23;
-//       while (q5 != q5answer){
-//         var q5 = prompt ("Nope! Guess again.");
-//         }
-//           score++;
-//           alert("That is correct!");
-//
-// //print score
-//       alert("You answered " + score + " questions correctly!");
